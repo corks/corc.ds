113d2904b18360a837f48ff1f078013936e591f3
Michael Rodriguez <michael@khaydarin.net>
2012-06-01 21:28:46 -0400

    Fix a CMake error, also change some things in bool.h to represent our style more

--- src/bool.h
+++ src/bool.h
@@ -1,11 +1,14 @@
 #ifndef LIBCAERUS_BOOL_H
 #define LIBCAERUS_BOOL_H
+
 #if (__STDC_VERSION__ < 199901L)
-typedef enum libcaerus_boolean_ {
-    false,
-    true
-} bool;
+    typedef enum libcaerus_boolean_
+    {
+        false,
+        true
+    } bool;
 #else
-#include <stdbool.h>
-#endif
+    #include <stdbool.h>
+#endif /* __STDC_VERSION__ < 199901L */
+
 #endif /* LIBCAERUS_BOOL_H */
